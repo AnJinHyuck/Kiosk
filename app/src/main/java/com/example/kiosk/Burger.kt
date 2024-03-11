e4c61e04a03d005db9f128dd0df7db5356043f20
Your Name <you@example.com>
2024-03-12 01:06:47 +0900

    second Kiosk

--- app/src/main/java/com/example/kiosk/Burger.kt
+++ app/src/main/java/com/example/kiosk/Burger.kt
@@ -1,27 +1,78 @@
 package com.example.kiosk
 
-class Burger(){
-    fun burgerMenu() {
-        println(
-            "[ Burgers MENU ]\n" +
-                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
-                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
-                    "3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거\n" +
-                    "4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
-                    "5. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
-                    "0. 뒤로가기      | 뒤로가기"
-        )
-        var burgerSelect = readln().toInt()
+interface MenuSelect {
+    fun menuItem(index: Int)
+}
 
+data class BurgerType(val name: String, val price: Double)
+
+//버거 정보 만들기
+class BurgerMenu : MenuSelect {
+
+    //버거 정보 넘기기
+    private val burgerList = listOf(
+        BurgerType("ShackBurger", 6.9),
+        BurgerType("Smoke Burger", 8.9),
+        BurgerType("Shroom Burger", 9.4),
+        BurgerType("Cheese Burger", 6.9),
+        BurgerType("HamBurger", 5.4),
+    )
+
+    override fun menuItem(index: Int) {
+
+        if (index in 1..burgerList.size) {
+            val selected = burgerList[index - 1]
+            println("${selected.name}을 선택하셨습니다.")
+
+        } else {
+            println("다시")
+        }
+    }
+
+    fun showMenu() {
+        println("[Bugers Menu]")
+        burgerList.forEachIndexed { index, burgerMenu ->
+            println("${index + 1}. ${burgerMenu.name}| W ${burgerMenu.price}|")
+        }
+        println("0. 뒤로가기")
+
+        val burgerSelect = readln().toInt()
         when (burgerSelect) {
-            1 -> println("를 선택하셨습니다 잠시만 기다려주세요")
-            2 -> println("SmokeShack를 선택하셨습니다 잠시만 기다려주세요")
-            3 -> println("Shroom Burger를 선택하셨습니다 잠시만 기다려주세요")
-            4 -> println("Cheesburger를 선택하셨습니다 잠시만 기다려주세요")
-            5 -> println("Hamburger를 선택하셨습니다 잠시만 기다려주세요")
             0 -> return
+            else -> menuItem(burgerSelect)
         }
     }
 }
+    //버거 선택
+//    override fun burgerMenu() {
+//        println("[ Burgers Menu")
+//        for(burgerList)
+////            "[ Burgers MENU ]\n" +
+////                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
+////                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
+////                    "3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거\n" +
+////                    "4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
+////                    "5. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
+////                    "0. 뒤로가기      | 뒤로가기"
+//
+//        var burgerSelect = readln().toInt()
+//
+//        when (burgerSelect) {
+//            1 -> println("를 선택하셨습니다 잠시만 기다려주세요")
+//            2 -> println("SmokeShack를 선택하셨습니다 잠시만 기다려주세요")
+//            3 -> println("Shroom Burger를 선택하셨습니다 잠시만 기다려주세요")
+//            4 -> println("Cheesburger를 선택하셨습니다 잠시만 기다려주세요")
+//            5 -> println("Hamburger를 선택하셨습니다 잠시만 기다려주세요")
+//            0 -> return
+//        }
+//    }
+
+
+
+
+// 0312 해야할 일. 다른 것도 클래스 화
+// 첫 화면에서 이상한 숫자 누르면 다시 입력하라는 메시지 출력
+// 다음 화면에서 이상한 숫자 누르면 다시 입력하라는 메시지 출력 - 첫 화면이 아니라 현재 화면에서 입력
+
 
 //0번
--- app/src/main/java/com/example/kiosk/MainScreen.kt
+++ app/src/main/java/com/example/kiosk/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.kiosk
 
+import android.view.Menu
 import kotlinx.coroutines.selects.select
 
 class Kiosk() {
@@ -7,26 +8,26 @@ class Kiosk() {
 }
 
 fun main() {
-
-    mainScreen()
+    println(mainScreen())
 }
 
 fun mainScreen() {
     while (true) {
         println(
             "[ SHAKESHACK MENU ]\n" +
-                    "1. Burgers         | 앵거스 비프 통살을 다져만든 버거\n" +
+                    "1. Burgers         | 버거\n" +
                     "2. Frozen Custard  | 매장에서 신선하게 만드는 아이스크림\n" +
-                    "3. Drinks          | 매장에서 직접 만드는 음료\n" +
-                    "4. Beer            | 뉴욕 브루클린 브루어리에서 양조한 맥주\n" +
+                    "3. Drinks          | 음료\n" +
+                    "4. Beer            | 맥주\n" +
                     "0. 종료            | 프로그램 종료"
         )
         println("원하는 메뉴를 선택하세요")
+
         var selectMenu = readln().toInt()
 
         when (selectMenu) {
 
-            1 -> Burger().burgerMenu()
+            1 -> BurgerMenu().showMenu()
             2 -> FrozenCustard().frozenCustard()
             3 -> Drinks().drinksMenu()
             4 -> Beer().beerMenu()
